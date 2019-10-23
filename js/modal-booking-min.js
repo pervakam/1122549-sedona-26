3d4d70e716edb1e60fa8386dd065e11cb4d08d7e
pervakam <52745442+pervakam@users.noreply.github.com>
2019-10-23 16:59:21 +0300

    оформ

--- js/modal-booking-min.js
+++ js/modal-booking-min.js
@@ -1,1 +1,19 @@
-var link=document.querySelector(".check-button"),popup=document.querySelector(".booking"),checkin=popup.querySelector("[name=check-in]");link.addEventListener("click",function(e){e.preventDefault(),popup.classList.toggle("modal-show"),checkin.focus()});
+var link = document.querySelector(".check-button");
+var	popup = document.querySelector(".booking");
+var form = popup.querySelector("form");
+var checkin = popup.querySelector("[name=check-in]");
+var checkout = popup.querySelector("[name=check-out]");
+var adults = popup.querySelector("[name=adults]");
+var childrens = popup.querySelector("[name=childrens]");
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+  checkin.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!checkin.value || !checkout.value || !adults.value || !childrens.value) {
+    evt.preventDefault();
+    console.log("Заполните все данные");
+  }
+});
