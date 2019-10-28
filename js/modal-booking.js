b2bd323b45db68f2c640b7bcd9e1f8c09b6a2e5c
pervakam <52745442+pervakam@users.noreply.github.com>
2019-10-28 16:46:35 +0300

    Правка после первой проверки

--- js/modal-booking.js
+++ js/modal-booking.js
@@ -5,15 +5,18 @@ var checkin = popup.querySelector("[name=check-in]");
 var checkout = popup.querySelector("[name=check-out]");
 var adults = popup.querySelector("[name=adults]");
 var childrens = popup.querySelector("[name=childrens]");
+
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-show");
+  popup.classList.toggle("modal-close");
   checkin.focus();
 });
 
 form.addEventListener("submit", function (evt) {
   if (!checkin.value || !checkout.value || !adults.value || !childrens.value) {
     evt.preventDefault();
-    console.log("Заполните все данные");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   }
 });
